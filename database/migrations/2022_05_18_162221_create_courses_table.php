9657e08340a0a5cbc92b53769a511f1fa1c4be3e
edgardbertelli <bertelliedgard@gmail.com>
2022-05-18 16:29:07 -0300

    Cria tabela de cursos

---
+++ database/migrations/2022_05_18_162221_create_courses_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCoursesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('courses', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->unsignedBigInteger('category_id');
+            $table->double('price');
+            $table->tinyText('author');
+            $table->time('length');
+            $table->timestamps();
+        });
+
+        Schema::table('courses', function (Blueprint $table) {
+            $table->foreign('category_id')->references('id')->on('categories');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('courses');
+    }
+}
