145264c5e3bb3da1fa2b9ce112d5f55dd6af2355
edgardbertelli <bertelliedgard@gmail.com>
2022-05-19 13:10:51 -0300

    Cria tabelas de generos e livros

---
+++ database/migrations/2022_05_19_154423_create_books_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBooksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('author');
+            $table->year('year');
+            $table->text('synopsis')->nullable();
+            $table->unsignedBigInteger('genre_id');
+            $table->double('price');
+            $table->timestamps();
+        });
+
+        Schema::table('books', function (Blueprint $table) {
+            $table->foreign('genre_id')->references('id')->on('genres');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+}
